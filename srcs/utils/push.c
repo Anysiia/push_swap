6f36ca2c3cbca74bea1846f936d3948ab32a1482
Anysia <anysia74@hotmail.com>
2021-03-22 15:21:51 +0100

    Only swap works, probably change all structuration of code

--- srcs/utils/push.c
+++ srcs/utils/push.c
@@ -6,7 +6,7 @@
 /*   By: cmorel-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:10:22 by cmorel-a          #+#    #+#             */
-/*   Updated: 2021/03/22 12:25:21 by cmorel-a         ###   ########.fr       */
+/*   Updated: 2021/03/22 15:15:27 by cmorel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,45 @@ int	push_a(t_all *all)
 {
 	int	ret;
 
-	if (!all->a->first)
-		return (EXIT_SUCCESS);
-	ret = push_front_number(all->b, all->a->first->value);
-	if (ret == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	remove_front_number(all->a);
-	all->a->len--;
-	all->b->len++;
+	if (all->b->len)
+	{
+		ret = push_front_number(all->a, all->b->first->value);
+		if (ret == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		remove_front_number(all->b);
+	}
 	return (EXIT_SUCCESS);
 }
 
 int	push_b(t_all *all)
 {
-	if (all->a->first)
-		all->a->first->prev = all->b->first;
-	else
-		all->a->last = all->b->first;
-	all->a->first = all->b->first;
-	all->b->first = all->b->first->next;
-	all->b->first->prev = NULL;
-	all->b->len--;
-	all->a->len++;
+	int	ret;
+
+	if (all->a->len)
+	{
+		ret = push_front_number(all->b, all->a->first->value);
+		if (ret == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		remove_front_number(all->a);
+	}
 	return (EXIT_SUCCESS);
 }
+
+/*
+static int	push(t_stack *src, t_stack *dst)
+{
+	int	value;
+
+	if (!src->first)
+		return (EXIT_SUCCESS);
+}
+
+int	push_b(t_all *all)
+{
+	return (push(all->a, all->b));
+}
+
+int	push_a(t_all *all)
+{
+	return (push(all->b, all->a));
+}*/
