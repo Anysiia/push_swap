288c1403d11834e978dac3af2d7f08831e214bc8
Anysia <anysia74@hotmail.com>
2021-03-23 12:43:13 +0100

    rotate && reverse rotate

--- srcs/utils/reverse_rotate.c
+++ srcs/utils/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: cmorel-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:18:05 by cmorel-a          #+#    #+#             */
-/*   Updated: 2021/03/18 16:25:19 by cmorel-a         ###   ########.fr       */
+/*   Updated: 2021/03/23 12:41:26 by cmorel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	reverse_rotate_a(t_all *all)
 {
+	if (all->a->len > 0)
+		all->a->first = all->a->first->prev;
 	return (EXIT_SUCCESS);
 }
 
 int	reverse_rotate_b(t_all *all)
 {
+	if (all->b->len > 0)
+		all->b->first = all->b->first->prev;
 	return (EXIT_SUCCESS);
 }
 
@@ -27,10 +31,6 @@ int	reverse_rotate_all(t_all *all)
 	int	ret;
 
 	ret = reverse_rotate_a(all);
-	if (ret == EXIT_FAILURE)
-		return (EXIT_FAILURE);
 	ret = reverse_rotate_b(all);
-	if (ret == EXIT_FAILURE)
-		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
--- srcs/utils/rotate.c
+++ srcs/utils/rotate.c
@@ -6,7 +6,7 @@
 /*   By: cmorel-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:15:28 by cmorel-a          #+#    #+#             */
-/*   Updated: 2021/03/18 16:25:35 by cmorel-a         ###   ########.fr       */
+/*   Updated: 2021/03/23 12:38:36 by cmorel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int	rotate_a(t_all *all)
 {
-
+	if (all->a->len > 0)
+		all->a->first = all->a->first->next;
 	return (EXIT_SUCCESS);
 }
 
 int	rotate_b(t_all *all)
 {
-
+	if (all->b->len > 0)
+		all->b->first = all->b->first->next;
 	return (EXIT_SUCCESS);
 }
 
@@ -29,10 +31,6 @@ int	rotate_all(t_all *all)
 	int	ret;
 
 	ret = rotate_a(all);
-	if (ret == EXIT_FAILURE)
-		return (EXIT_FAILURE);
 	ret = rotate_b(all);
-	if (ret == EXIT_FAILURE)
-		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
