467a1865c1fefd36fa10c3ef01280b8a38f38511
Anysia <anysia74@hotmail.com>
2021-05-25 10:34:16 +0200

    Fix bug on redir in a directory

--- srcs/checker/instruction.c
+++ srcs/checker/instruction.c
@@ -6,7 +6,7 @@
 /*   By: cmorel-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 17:05:40 by cmorel-a          #+#    #+#             */
-/*   Updated: 2021/04/15 15:22:45 by cmorel-a         ###   ########.fr       */
+/*   Updated: 2021/05/25 10:33:30 by cmorel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	get_instruction(t_all *all)
 	char	*line;
 	int		ret;
 
+	line = NULL;
 	while (get_next_line(STDIN, &line) == 1)
 	{
 		ret = is_instruction(line);
@@ -49,7 +50,8 @@ void	get_instruction(t_all *all)
 		}
 		ft_freestr(&line);
 	}
-	ft_freestr(&line);
+	if (line)
+		ft_freestr(&line);
 }
 
 void	execute_all_instruction(t_all *all)
